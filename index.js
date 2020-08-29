1fbf95ce62e4c688fbb4e02352f26b55854ac24c
K Chingsubam <kchings1@binghamton.edu>
2020-08-28 21:08:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ io.on('connect', socket => {
   });
 });
 
+
+const socket = io('ws://localhost:3000');
+
+socket.on('connect', () => {
+  // either with send()
+  socket.send('Hello!');
+
+  // or with emit() and custom event names
+  socket.emit('salutations', 'Hello!', { 'mr': 'john' }, Uint8Array.from([1, 2, 3, 4]));
+});
+
+// handle the event sent with socket.send()
+socket.on('message', data => {
+  console.log(data);
+});
+
+
+
 app.use(express.static("public"));
 
 //Socket
