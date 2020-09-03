b2a587d14568c2db5c24d74d22637c50be5fc5b5
Kajal Chingsubam <kchings1@binghamton.edu>
2020-09-02 23:19:53 -0400

    implementing scroll fn

--- public/chat.js
+++ public/chat.js
@@ -40,3 +40,31 @@ socket.on("typing", (data_1) => {
   console.log("data for userid : ", data_1);
   feedback.innerHTML = data_1 + " is typing...";
 });
+
+function appendMessage() {
+  const message = document.getElementsByClassName("message")[0];
+  const newMessage = message.cloneNode(true);
+  messages.appendChild(newMessage);
+}
+
+function getMessages() {
+  // Prior to getting your messages.
+  shouldScroll =
+    messages.scrollTop + messages.clientHeight === messages.scrollHeight;
+  /*
+   * Get your messages, we'll just simulate it by appending a new one syncronously.
+   */
+  appendMessage();
+  // After getting your messages.
+  if (!shouldScroll) {
+    scrollToBottom();
+  }
+}
+
+function scrollToBottom() {
+  messages.scrollTop = messages.scrollHeight;
+}
+
+scrollToBottom();
+
+setInterval(getMessages, 100);
